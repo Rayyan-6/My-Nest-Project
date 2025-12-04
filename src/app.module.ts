1fbfca013cedb136e998f66c8128c36f75e54729
Muhammad Rayyan <muhd.rayyan.mail@gmail.com>
2025-12-04 13:31:15 +0500

    PostgreSql configurated in the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'db/data-source';
 
 
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nestjs_tutorial'),
-    UserModule
+    // MongooseModule.forRoot('mongodb://127.0.0.1:27017/nestjs_tutorial'),
+    // UserModule
+    TypeOrmModule.forRoot(dataSourceOptions)
   ],
   controllers: [AppController],
   providers: [AppService],
